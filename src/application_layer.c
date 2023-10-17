e2ebc70ece2c19f4b535bea746263eb7b60bc1fd
Rodrigo Póvoa <rpovoa29@gmail.com>
2023-10-17 23:00:23 +0100

    did buildcontrolpacket

--- src/application_layer.c
+++ src/application_layer.c
@@ -6,6 +6,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <math.h>
+
+
+
+unsigned char *buildControlPacket(const int C, long int filesize, const char *filename, int *size){
+
+    int pointer = 0;
+
+    int L1 = ceil((float)filesize/8);
+
+    int L2 = strlen(filename);
+
+    *size = (1) + (2+L1) + (2+L2);
+
+    unsigned char *controlPacket = malloc(*size * sizeof(unsigned char));
+
+    controlPacket[pointer++] = C;
+    controlPacket[pointer++] = 0;
+    controlPacket[pointer] = L1;
+    
+    
+    for(char i=0;i<L1;){
+        controlPacket[pointer + L1 + i] = filesize;
+        filesize>>=8;
+    }
+
+    pointer+=L1+1;
+
+    controlPacket[pointer++]=1;
+    controlPacket[pointer++]=L2;
+    memcpy(controlPacket+pointer,filename,L2);
+    return controlPacket;
+
+}
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -32,21 +67,48 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     {
     case LlTx:
         
+        //Open file
+
         FILE *file = fopen(filename,"rb");
         if(file==NULL){
             perror("File doesn't exist.");
             exit(-1);
         }
 
+
+        //Get file size
+
         fseek(file, 0, SEEK_END);
         long fileSize = ftell(file);
         fseek(file,0,SEEK_SET);
         printf("%ld\n",fileSize);
 
+        //Send Control Packet START
+
+        int controlSize;
+        unsigned char *ControlPacketStart = buildControlPacket(2,fileSize,filename,&controlSize);
+        llwrite(ControlPacketStart,controlSize);
+
+
+        //Send Data Packets
+
+            //...
+
+
+        //Send Control Packet END
+        
+            //...
+
+
         break;
     
     case LlRx:
 
+        //Receive Control Packet START
+
+        //Receive Data Packets
+
+        //Close
         break;
     }
 }
